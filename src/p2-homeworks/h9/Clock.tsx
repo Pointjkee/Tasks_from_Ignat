f82051b70e2a0a7b312f8c2281e1ddc59c4a8909
Denis <razoranet@gmail.com>
2021-10-20 19:24:42 +0300

    lesson #8 clock

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,35 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
+const moment = require('moment');
+
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
     const [date, setDate] = useState<Date>()
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
+        clearInterval(timerId)
         // stop
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
+            setDate(new Date())
             // setDate
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        return setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        return setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() // fix with date
+    const stringDate = date?.toLocaleDateString() // fix with date
 
     return (
         <div>
